f65903de85e0147874fbe17646d28a81b12acfa5
fatemamanakh <fatemamanakh25@gmail.com>
2024-04-13 18:27:05 -0400

    Add static website

--- cicd-s3.tf
+++ cicd-s3.tf
@@ -4,12 +4,26 @@ resource "aws_s3_bucket" "codepipeline_artifacts" {
 } 
 
 resource "aws_s3_bucket" "static_website_bucket" {
-  bucket = "my-static-website-bucket-fatema"  # Replace with your desired bucket name
-  acl    = "public-read"  # Allow public read access to the bucket
+  bucket = "my-static-website-fatema"  # Update with your desired bucket name
 
   website {
     index_document = "index.html"
-    error_document = "error.html"
   }
 }
 
+resource "aws_s3_bucket_policy" "static_website_bucket_policy" {
+  bucket = aws_s3_bucket.static_website_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid = "PublicReadGetObject",
+        Effect = "Allow",
+        Principal = "*",
+        Action = ["s3:GetObject"],
+        Resource = aws_s3_bucket.static_website_bucket.arn,
+      },
+    ],
+  })
+}
