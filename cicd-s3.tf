5076de32e355037edbdb5c40b4455cc2f0d73a8c
fatemamanakh <fatemamanakh25@gmail.com>
2024-04-13 18:21:53 -0400

    Add static website

--- cicd-s3.tf
+++ cicd-s3.tf
@@ -2,3 +2,14 @@ resource "aws_s3_bucket" "codepipeline_artifacts" {
   bucket = "pipeline-artifacts-fatema"
   acl    = "private"
 } 
+
+resource "aws_s3_bucket" "static_website_bucket" {
+  bucket = "my-static-website-bucket-fatema"  # Replace with your desired bucket name
+  acl    = "public-read"  # Allow public read access to the bucket
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}
+
