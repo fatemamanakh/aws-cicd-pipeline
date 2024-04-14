21c8d7ce06d21464a84c0338c89ffcec2630bafb
fatemamanakh <fatemamanakh25@gmail.com>
2024-04-13 22:53:03 -0400

    Add static website

--- cicd-s3.tf
+++ cicd-s3.tf
@@ -19,10 +19,14 @@ resource "aws_s3_bucket_object" "website_objects" {
   bucket = aws_s3_bucket.static_website_bucket.id
 
   # Specify the source directory containing your static website content
-  # Here, assuming the website content is located in the "website" directory
-  source = "./website"
+  # Update the path to reflect the correct relative path to the website directory
+  source = "./website/"
 
   # Specify the destination directory in the S3 bucket
-  # Since the key attribute is set to ".", the content will be uploaded to the root directory of the bucket
-  key    = "."
+  # Here, assuming the content should be placed at the root of the bucket
+  # Adjust the "key" attribute as needed to place the content in a subdirectory
+  key    = ""
+
+  # Set ACL to public-read for public access
+  acl    = "public-read"
 }
