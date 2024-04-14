331d266d9bcd8c902edc9c6b00f898dba182c8b2
fatemamanakh <fatemamanakh25@gmail.com>
2024-04-14 00:33:24 -0400

    Add static website

--- cicd-s3.tf
+++ cicd-s3.tf
@@ -6,7 +6,6 @@ resource "aws_s3_bucket" "codepipeline_artifacts" {
 # Define the S3 bucket for hosting the static website
 resource "aws_s3_bucket" "bucket" {
   bucket = "my-static-website-fatema001"
-  acl    = "public-read"
 
   website {
     index_document = "index.html"
@@ -20,4 +19,20 @@ resource "aws_s3_bucket_object" "file_upload" {
     key = each.value
     source = "${path.module}/static/${each.value}"
     etag = filemd5("${path.module}/static/${each.value}") 
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.bucket.arn}/*"
+      }
+    ]
+  })
 }
