d7c27f20a65019f23e2c4e7591fcc594eaa983a9
fatemamanakh <fatemamanakh25@gmail.com>
2024-04-13 23:40:49 -0400

    Add static website

--- cicd-s3.tf
+++ cicd-s3.tf
@@ -5,13 +5,9 @@ resource "aws_s3_bucket" "codepipeline_artifacts" {
 
 # Define the S3 bucket for hosting the static website
 resource "aws_s3_bucket" "static_website_bucket" {
-  bucket = "my-static-website-fatema"  # Update with your desired bucket name
+  bucket = "my-static-website-fatema"
   acl    = "public-read"
 
-  provisioner "local-exec" {
-    command = "aws s3 sync static/ s3://${aws_s3_bucket.static_website_bucket} --acl public-read --delete"
-    
-  }
   # Set the index document for the bucket
   # This will serve index.html as the default document
   website {
@@ -19,3 +15,18 @@ resource "aws_s3_bucket" "static_website_bucket" {
   }
 }
 
+# Define the bucket policy
+resource "aws_s3_bucket_policy" "static_website_bucket_policy" {
+  bucket = aws_s3_bucket.static_website_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Sid       = "PublicReadGetObject",
+      Effect    = "Allow",
+      Principal = "*",
+      Action    = "s3:GetObject",
+      Resource  = "${aws_s3_bucket.static_website_bucket.arn}/*"
+    }]
+  })
+}
