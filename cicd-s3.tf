4f3c1058ed919306ef193250498ee81cffc389e5
fatemamanakh <fatemamanakh25@gmail.com>
2024-04-13 22:59:57 -0400

    Add static website

--- cicd-s3.tf
+++ cicd-s3.tf
@@ -6,7 +6,12 @@ resource "aws_s3_bucket" "codepipeline_artifacts" {
 # Define the S3 bucket for hosting the static website
 resource "aws_s3_bucket" "static_website_bucket" {
   bucket = "my-static-website-fatema"  # Update with your desired bucket name
+  acl    = "public-read"
 
+  provisioner "local-exec" {
+    command = "aws s3 sync static/ s3://${aws_s3_bucket.static_website_bucket} --acl public-read --delete"
+    
+  }
   # Set the index document for the bucket
   # This will serve index.html as the default document
   website {
@@ -14,19 +19,3 @@ resource "aws_s3_bucket" "static_website_bucket" {
   }
 }
 
-# Upload static website content to the S3 bucket
-resource "aws_s3_bucket_object" "website_objects" {
-  bucket = aws_s3_bucket.static_website_bucket.id
-
-  # Specify the source directory containing your static website content
-  # Update the path to reflect the correct relative path to the website directory
-  source = "./website/"
-
-  # Specify the destination directory in the S3 bucket
-  # Here, assuming the content should be placed at the root of the bucket
-  # Adjust the "key" attribute as needed to place the content in a subdirectory
-  key    = ""
-
-  # Set ACL to public-read for public access
-  acl    = "public-read"
-}
