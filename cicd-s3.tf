69a342277be1c9c11b516f05f08f858152e6bdb6
fatemamanakh <fatemamanakh25@gmail.com>
2024-04-14 00:23:40 -0400

    Add static website

--- cicd-s3.tf
+++ cicd-s3.tf
@@ -4,34 +4,20 @@ resource "aws_s3_bucket" "codepipeline_artifacts" {
 } 
 
 # Define the S3 bucket for hosting the static website
-resource "aws_s3_bucket" "static_website_bucket" {
-  bucket = "my-static-website-fatema"  # Update with your desired bucket name
+resource "aws_s3_bucket" "bucket" {
+  bucket = "my-static-website-fatema"
   acl    = "public-read"
 
-  provisioner "local-exec" {
-  command = "aws s3 sync static/ s3://${aws_s3_bucket.static_website_bucket.bucket} --acl public-read --delete"
-}
-
-  # Set the index document for the bucket
-  # This will serve index.html as the default document
   website {
     index_document = "index.html"
   }
+  
 }
 
-
-# Define the bucket policy
-resource "aws_s3_bucket_policy" "static_website_bucket_policy" {
-  bucket = aws_s3_bucket.static_website_bucket.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Sid       = "PublicReadGetObject",
-      Effect    = "Allow",
-      Principal = "*",
-      Action    = "s3:GetObject",
-      Resource  = "${aws_s3_bucket.static_website_bucket.arn}/*"
-    }]
-  })
+resource "aws_s3_bucket_object" "file_upload" {
+  for_each = fileset("${path.module}/static", "**")
+    bucket = aws_s3_bucket.bucket.id
+    key = each.value
+    source = "${path.module}/static/${each.value}"
+    etag = filemd5("${path.module}/static/${each.value}") 
 }
