5ab8838e0e2a878fc43a216af6d2d8afa2999b8e
fatemamanakh <fatemamanakh25@gmail.com>
2024-04-16 15:21:24 -0400

    Add static website

--- cicd-s3.tf
+++ cicd-s3.tf
@@ -4,35 +4,35 @@ resource "aws_s3_bucket" "codepipeline_artifacts" {
 } 
 
 # Define the S3 bucket for hosting the static website
-resource "aws_s3_bucket" "bucket" {
-  bucket = "my-static-website-fatema001"
+# resource "aws_s3_bucket" "bucket" {
+#   bucket = "my-static-website-fatema001"
 
-  website {
-    index_document = "index.html"
-  }
+#   website {
+#     index_document = "index.html"
+#   }
   
-}
+# }
 
-resource "aws_s3_bucket_object" "file_upload" {
-  for_each = fileset("${path.module}/static", "**")
-    bucket = aws_s3_bucket.bucket.id
-    key = each.value
-    source = "${path.module}/static/${each.value}"
-    etag = filemd5("${path.module}/static/${each.value}") 
-}
+# resource "aws_s3_bucket_object" "file_upload" {
+#   for_each = fileset("${path.module}/static", "**")
+#     bucket = aws_s3_bucket.bucket.id
+#     key = each.value
+#     source = "${path.module}/static/${each.value}"
+#     etag = filemd5("${path.module}/static/${each.value}") 
+# }
 
-resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = aws_s3_bucket.bucket.id
+# resource "aws_s3_bucket_policy" "bucket_policy" {
+#   bucket = aws_s3_bucket.bucket.id
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect    = "Allow",
-        Principal = "*",
-        Action    = "s3:GetObject",
-        Resource  = "${aws_s3_bucket.bucket.arn}/*"
-      }
-    ]
-  })
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect    = "Allow",
+#         Principal = "*",
+#         Action    = "s3:GetObject",
+#         Resource  = "${aws_s3_bucket.bucket.arn}/*"
+#       }
+#     ]
+#   })
+# }
